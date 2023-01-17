a42fc086b24d30ec3bf1d229a8703f8be97504c1
Lucas Colwell <colwellc37@gmail.com>
2023-01-16 21:36:12 -0700

    Learning resource endpoint sad path

--- spec/requests/get_learning_resources_spec.rb
+++ spec/requests/get_learning_resources_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Get recipe', vcr: {record: :new_episodes} do
+RSpec.describe 'Get recipe', :vcr do
   describe "happy path" do
     it 'retrieves learning resources for a given country' do
       country = ("thailand")
@@ -17,6 +17,20 @@ RSpec.describe 'Get recipe', vcr: {record: :new_episodes} do
       expect(parsed[:data][:attributes][:video]).to be_a Hash
       expect(parsed[:data][:attributes][:video][:title]).to be_a String
       expect(parsed[:data][:attributes][:video][:youtube_video_id]).to be_a String
+    end
+  end
+
+  context "sad path" do
+    it "returns empty objects in same format if no media found" do
+      country = ("notarealcountry")
+
+      get "/api/v1/learning_resources?country=#{country}"
+      parsed = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed[:data].keys).to eq([:id, :type, :attributes])
+      expect(parsed[:data][:attributes].keys).to eq([:country, :video, :images])
+      expect(parsed[:data][:attributes][:images]).to eq []
+      expect(parsed[:data][:attributes][:video]).to eq []
     end
   end
 end
