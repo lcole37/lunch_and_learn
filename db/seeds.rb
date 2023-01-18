d3a1d2dc6910498f40ef1d421ed90afa0b7e84b5
Lucas Colwell <colwellc37@gmail.com>
2023-01-17 22:46:48 -0700

    Add favorite seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: "Happy", email: "example@happyemail.com", api_key: "012345")
+user1 = User.create!(name: "Happy", email: "example@happyemail.com", api_key: "012345")
+
+favorite1 = user1.favorites.create!(country: "Italy", recipe_link: "https//www.allrecipes.com", recipe_title: "Italian Recipe")
+favorite2 = user1.favorites.create!(country: "Morocco", recipe_link: "https//www.goodeats.com", recipe_title: "Moroccan Recipe")
